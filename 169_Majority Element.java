bc968111b636e2ce08215c15fd434c43967df5b1
yamagao <yamagao@126.com>
2015-05-05 10:46:12 -0400

    05042015

---
+++ 169_Majority Element.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int val = nums[0];
+        int count = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(count == 0){
+                val = nums[i];
+                count++;
+                continue;
+            }
+            else if(val == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return val;
+    }
+}
