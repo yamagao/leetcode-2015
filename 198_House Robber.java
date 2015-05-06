d8adc469325c0f77f8ecec096b3e4ca909829f3e
yamagao <yamagao@126.com>
2015-05-06 12:32:59 -0400

    05062015

---
+++ 198_House Robber.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        if(nums.length == 1){
+            return nums[0];
+        }
+        if(nums.length > 2){
+            nums[2] += nums[0];
+        }
+        for(int i = 3; i < nums.length; i++){
+            nums[i] += Math.max(nums[i - 2], nums[i - 3]);
+        }
+        return Math.max(nums[nums.length - 1], nums[nums.length - 2]);
+    }
+}
